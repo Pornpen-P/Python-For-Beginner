4de16a9ee36c7a85cd69c526aa85d9c501bef050
Pornpen -P <pornpen@welovebug.biz>
2024-04-25 10:28:46 +0700

    [Created]: สร้างฟังก์ชันทักทายชื่อสาว ๆ WLB

--- functions.py
+++ functions.py
@@ -1,8 +1,18 @@
-#นิยาม
+#นิยามฟังก์ชัน
 def greet_pook():
     print('Hello Pook')
 
 #เรียกใช้ (call)
 greet_pook()
 
-    
+
+def greet(name):
+     print('Hello '+name)
+
+greet('Jackie')
+greet('Por')
+greet('Ple')
+greet('Bumebim')
+greet('Nan')
+greet('Kaofang')
+greet('Tonmint')
